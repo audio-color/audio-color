ca13587fc6c9da907cac478999d4114d08ef64b9
Jeremy Lee <www.jeremy.llee27@gmail.com>
2019-08-29 23:02:38 -0700

    Pushing this shit up

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-'use strict';
-const rgb = require('./rgb.js');
+"use strict";
+// const rgb = require('./rgb.js');
 
 const range = (start, end) => {
   const length = end - start;
   return Array.from({ length }, (_, i) => start + i);
-}
+};
 
-const validateParam = function (num) {
-  if (num < 0 || num > 10 || typeof num !== 'number') {
-    throw 'invalid parameter';
-  }
-  else {
-    console.log('good value', num)
+const validateParam = function(num) {
+  console.log(num);
+  if (num < 0 || num > 10 || typeof num !== "number") {
+    console.log("in if");
+    return "invalid parameter";
+  } else {
+    console.log("good value", num);
     return true;
   }
-}
+};
 
-const convertMoodToRGB = (valence) => {
-
-  validateParam(valence)
+const convertMoodToRGB = valence => {
+  validateParam(valence);
 
   let groupOne = range(0, 4);
   let groupTwo = range(4, 7);
   let groupThree = range(7, 11);
 
-  if (groupOne.includes(valence)) {
-    console.log('group one');
-    rgb.rgbOne()
-  }
-  if (groupTwo.includes(valence)) {
-    console.log('group two');
-    rgb.rgbTwo()
-  }
-  if (groupThree.includes(valence)) {
-    console.log('group three');
-    rgb.rgbThree()
-  }
+  // if (groupOne.includes(valence)) {
+  //   console.log('group one');
+  //   rgb.rgbOne()
+  // }
+  // if (groupTwo.includes(valence)) {
+  //   console.log('group two');
+  //   rgb.rgbTwo()
+  // }
+  // if (groupThree.includes(valence)) {
+  //   console.log('group three');
+  //   rgb.rgbThree()
+  // }
 };
 
-module.exports = {range, validateParam, convertMoodToRGB};
+module.exports = { range, validateParam, convertMoodToRGB };
